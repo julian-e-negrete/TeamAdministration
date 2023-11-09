2b52422c96882f8e642495351315ed6877f8b629
Julian Negrete <julian.negrete.profesional@gmail.com>
2023-11-09 17:34:12 -0300

    Fix Webconfig connection

--- WebForm-CSharp/Global.asax.cs
+++ WebForm-CSharp/Global.asax.cs
@@ -16,32 +16,32 @@ namespace WebForm_CSharp
     public class Global : HttpApplication
     {
 
-        private IServiceCollection serviceCollection;
-        private ServiceProvider serviceProvider; 
+        //private IServiceCollection serviceCollection;
+        //private ServiceProvider serviceProvider; 
         void Application_Start(object sender, EventArgs e)
         {
             // Code that runs on application startup
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
-            // Create a service collection.
-            var serviceCollection = new ServiceCollection();
+            //// Create a service collection.
+            //var serviceCollection = new ServiceCollection();
 
-            // Add the hosted service to the service collection.
-            serviceCollection.AddHostedService<MyBackgroundService>();
+            //// Add the hosted service to the service collection.
+            //serviceCollection.AddHostedService<MyBackgroundService>();
 
-            // Build the service provider.
-            var serviceProvider = serviceCollection.BuildServiceProvider();
+            //// Build the service provider.
+            //var serviceProvider = serviceCollection.BuildServiceProvider();
 
-            // Start the background service.
-            serviceProvider.GetService<IHostedService>().StartAsync(CancellationToken.None).GetAwaiter().GetResult();
+            //// Start the background service.
+            //serviceProvider.GetService<IHostedService>().StartAsync(CancellationToken.None).GetAwaiter().GetResult();
         }
 
-        protected void Application_End(object sender, EventArgs e)
-        {
-            // Dispose of the service provider.
-            serviceProvider.Dispose();
-        }
+        //protected void Application_End(object sender, EventArgs e)
+        //{
+        //    // Dispose of the service provider.
+        //    serviceProvider.Dispose();
+        //}
 
     }
 }
